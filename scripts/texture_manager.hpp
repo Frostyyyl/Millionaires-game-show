3dcd7a61cb5a748e7dc4a8d779bc26ec34e634a0
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2023-12-09 22:47:12 +0100

    Moved TextureManager to one file

--- scripts/texture_manager.hpp
+++ scripts/texture_manager.hpp
@@ -5,6 +5,18 @@
 
 class TextureManager{
 public:
-    static SDL_Texture* LoadTexture(const char* filename);
-    static void Draw(SDL_Texture* tex, SDL_Rect src, SDL_Rect dest);
+    static SDL_Texture* LoadTexture(const char* filename){
+        SDL_Surface* tempSurface = IMG_Load(filename);
+        SDL_Texture* tex = SDL_CreateTextureFromSurface(Game::renderer, tempSurface);
+        if(tex == nullptr){
+            std::cerr << "Couldn't load texture" << std::endl;
+            exit(1);
+        }
+        SDL_FreeSurface(tempSurface);
+
+        return tex;
+    }
+    static void Draw(SDL_Texture* tex, SDL_Rect &src, SDL_Rect &dest){
+        SDL_RenderCopy(Game::renderer, tex, &src, &dest);
+    }
 };
