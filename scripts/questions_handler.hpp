43f0175a0f5e0dd414af7f0d98e048d9a37efca7
Mikołaj Kiszka <mikolajkiszka123@gmail.com>
2023-12-26 00:16:25 +0100

    Modified questions system

--- scripts/questions_handler.hpp
+++ scripts/questions_handler.hpp
@@ -10,17 +10,30 @@ struct Question {
     std::string question;
     std::string correctAnswer;
     std::string incorrectAnswers[3];
-    std::string tier;
 };
 
-std::vector<struct Question> loadJSON(const std::string filename){
-    std::vector<struct Question> questions;
+/*
+    loadJSON returns all questions:
+    easy questions - questions[0]
+    medium questions - questions[1]
+    hard questions - questions[2]
+*/
+std::vector<std::vector<struct Question>> loadJSON(const std::string filename){
+    std::vector<std::vector<struct Question>> questions(3);
     std::ifstream jsonFile(filename);
     if(jsonFile.is_open()){
         json j = json::parse(jsonFile);
-        for (int i = 0; i < j.size(); i++){
-            Question q = {.question = j[i]["question"], .correctAnswer = j[i]["correct_answer"], .incorrectAnswers = {j[i]["incorrect_answers"][0], j[i]["incorrect_answers"][1], j[i]["incorrect_answers"][2]}, .tier = j[i]["tier"]};
-            questions.push_back(q);
+        for (int i = 0; i < j["easy"].size(); i++){
+            Question q = {.question = j["easy"][i]["question"], .correctAnswer = j["easy"][i]["correct_answer"], .incorrectAnswers = {j["easy"][i]["incorrect_answers"][0], j["easy"][i]["incorrect_answers"][1], j["easy"][i]["incorrect_answers"][2]}};
+            questions[0].push_back(q);
+        }
+        for (int i = 0; i < j["medium"].size(); i++){
+            Question q = {.question = j["medium"][i]["question"], .correctAnswer = j["medium"][i]["correct_answer"], .incorrectAnswers = {j["medium"][i]["incorrect_answers"][0], j["medium"][i]["incorrect_answers"][1], j["medium"][i]["incorrect_answers"][2]}};
+            questions[1].push_back(q);
+        }
+        for (int i = 0; i < j["hard"].size(); i++){
+            Question q = {.question = j["hard"][i]["question"], .correctAnswer = j["hard"][i]["correct_answer"], .incorrectAnswers = {j["hard"][i]["incorrect_answers"][0], j["hard"][i]["incorrect_answers"][1], j["hard"][i]["incorrect_answers"][2]}};
+            questions[2].push_back(q);
         }
         return questions;
     }else{
