9a2cab9b60bcdfda368107920e2e1313cdb64534
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2023-12-26 15:08:04 +0100

    Started working with TTF

--- scripts/game.cpp
+++ scripts/game.cpp
@@ -1,9 +1,11 @@
 #include "game.hpp"
+#include "SDL_image.h"
+#include "SDL_ttf.h"
 #include "object_system.hpp"
 #include "sprite_object.hpp"
 #include "input_manager.hpp"
 #include "button_object.hpp"
-
+//#include "text_sprite.hpp"
 
 SDL_Renderer* Game::renderer = nullptr;
 SDL_Event Game::event;
@@ -21,23 +23,39 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height){
         std::cout << "Game init error" << std::endl;
         exit(1);
     }
+    if(IMG_Init(IMG_INIT_PNG) < 0){
+        std::cout << "IMG_Init error: " << IMG_GetError() << std::endl;
+        isRunning = false;
+        exit(1);
+    }
+    std::cout << TTF_GetError() << std::endl;
+    //this part crashes whole program idk why
+    // if (TTF_Init() < 0) {
+    //     std::cout << "TTF_Init error" << std::endl;
+    //     // Handle initialization failure
+    //     isRunning = false;
+    //     exit(1);
+    // } else {
+    //     std::cout << "TTF_Init successful!" << std::endl;
+    // }
 
     window = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN);
     if (window == NULL){
         std::cout << "Window creation error" << std::endl;
         exit(1);
     }
-
+    
     renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     isRunning = true;
 
     // here is creating and adding objects to ObjectManager and InputManager
     // will probably need to change this later so it's more clear
-    Sprite* sprite = new Sprite("images/button_spritesheet.png", 100, 20, 2, 1);
+    
     Button* button = new Button("images/button_spritesheet.png", 100, 300, 2, 1);
-    objectManager.addObject(sprite);
+    //TextSprite* text = new TextSprite("images/button_spritesheet.png", 100, 20, "a.ttf", "lol", 1, 1);
     objectManager.addObject(button);
+    //objectManager.addObject(text);
     inputManager.addButton(button);
 }
 
@@ -70,6 +88,8 @@ void Game::clean(){
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
     SDL_Quit();
+    //TTF_Quit();
+    //IMG_Quit();
 }
 
 //for now not used, want to add this to inputManager
---
+++ temp/text_manager.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "SDL.h"
+//#include "SDL_TTF.h"
+#include "game.hpp"
+
+class TextManager{
+public:
+    // static TTF_Font* LoadFont(const char* font_filename){
+    //     std::cout << "ohio" << std::endl;
+    //     TTF_Font* font = TTF_OpenFont(font_filename, 20);
+    //     if (!font) {
+    //         std::cerr << "Failed to load font: " << TTF_GetError() << std::endl;
+    //     }
+    //     return font;
+    // }
+};
