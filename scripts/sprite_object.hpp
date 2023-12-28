ed5120f00a073b73bb0d66e5547e253b754ce1c4
Frostyl <lgrobelny66@gmail.com>
2023-12-28 17:21:12 +0100

    Separated sprite and spritesheet objects

--- scripts/sprite_object.hpp
+++ scripts/sprite_object.hpp
@@ -7,22 +7,17 @@ class Sprite : public Object{
 private:
     Position pos;
     int width, height; 
-    int spritesheetWidth, spritesheetHeight;
     SDL_Rect src, dest;
     SDL_Texture* tex;
 public:
-    // would be cool if we could get rid of these         V                      V
-    Sprite(const char* filename, int x, int y, int spritesheetWidth = 1, int spritesheetHeight = 1) : pos(x, y){
-        this->spritesheetWidth = spritesheetWidth;
-        this->spritesheetHeight = spritesheetHeight;
+    Sprite(const char* filename, int x, int y) : pos(x, y){
         tex = TextureManager::LoadTexture(filename);
         SDL_QueryTexture(tex, NULL, NULL, &width, &height);
-        src.x = (width / spritesheetWidth) * 0;    // those zeros represent the spritesheet index of a loaded texture 
-        src.y = (height / spritesheetHeight) * 0;  // default is 0
+        src.x = src.y = 0; 
         dest.x = pos.getX();
         dest.y = pos.getY();
-        src.w = dest.w = width / spritesheetWidth;
-        src.h = dest.h = height / spritesheetHeight;
+        src.w = dest.w = width;
+        src.h = dest.h = height;
     }
     /* this is probably not needed now
     Sprite(int x, int y) : pos(x, y){
@@ -37,6 +32,36 @@ public:
     void draw() override{
         TextureManager::Draw(tex, src, dest);
     }
+    void update() override{
+        dest.x = pos.getX();
+        dest.y = pos.getY();
+    };
+};
+
+class Spritesheet : public Object{
+private:
+    Position pos;
+    int width, height; 
+    int spritesheetWidth, spritesheetHeight;
+    SDL_Rect src, dest;
+    SDL_Texture* tex;
+public:
+    Spritesheet(const char* filename, int x, int y, int spritesheetWidth, int spritesheetHeight) : pos(x, y){
+        this->spritesheetWidth = spritesheetWidth;
+        this->spritesheetHeight = spritesheetHeight;
+        tex = TextureManager::LoadTexture(filename);
+        SDL_QueryTexture(tex, NULL, NULL, &width, &height);
+        src.x = (width / spritesheetWidth) * 0;     // during inicialization we set the sprite to first of a spritesheet  
+        src.y = (height / spritesheetHeight) * 0;
+        dest.x = pos.getX();
+        dest.y = pos.getY();
+        src.w = dest.w = width / spritesheetWidth;
+        src.h = dest.h = height / spritesheetHeight;
+    }
+    void draw() override{
+        TextureManager::Draw(tex, src, dest);
+    }
+    // NOTE: sprite update of a spritesheet should be implemented below 
     void update() override{
         dest.x = pos.getX();
         dest.y = pos.getY();
