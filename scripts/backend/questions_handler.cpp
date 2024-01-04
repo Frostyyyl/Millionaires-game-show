22c37a43a06778f0c1e4f864fabe6052d9f7b9d9
Mikołaj Kiszka <mikolajkiszka123@gmail.com>
2024-01-04 20:59:07 +0100

    Fixed bugs with double loadingJSON declaration

--- scripts/backend/questions_handler.cpp
+++ scripts/backend/questions_handler.cpp
@@ -3,6 +3,32 @@
 unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 std::mt19937 g(seed);
 
+QuestionsHandler* QuestionsHandler::Instance = nullptr;
+
+std::vector<std::vector<struct Question>> loadJSON(const std::string filename){
+    std::vector<std::vector<struct Question>> questions(3);
+    std::ifstream jsonFile(filename);
+    if(jsonFile.is_open()){
+        json j = json::parse(jsonFile);
+        for (int i = 0; i < j["easy"].size(); i++){
+            Question q = {.question = j["easy"][i]["question"], .correctAnswer = j["easy"][i]["correct_answer"], .incorrectAnswers = {j["easy"][i]["incorrect_answers"][0], j["easy"][i]["incorrect_answers"][1], j["easy"][i]["incorrect_answers"][2]}};
+            questions[0].push_back(q);
+        }
+        for (int i = 0; i < j["medium"].size(); i++){
+            Question q = {.question = j["medium"][i]["question"], .correctAnswer = j["medium"][i]["correct_answer"], .incorrectAnswers = {j["medium"][i]["incorrect_answers"][0], j["medium"][i]["incorrect_answers"][1], j["medium"][i]["incorrect_answers"][2]}};
+            questions[1].push_back(q);
+        }
+        for (int i = 0; i < j["hard"].size(); i++){
+            Question q = {.question = j["hard"][i]["question"], .correctAnswer = j["hard"][i]["correct_answer"], .incorrectAnswers = {j["hard"][i]["incorrect_answers"][0], j["hard"][i]["incorrect_answers"][1], j["hard"][i]["incorrect_answers"][2]}};
+            questions[2].push_back(q);
+        }
+        return questions;
+    }else{
+        std::cout << "Error. Couldn't open file: " << filename << std::endl;
+        return questions;
+    }
+}
+
 void QuestionsHandler::drawQuestion(){
     std::uniform_int_distribution<int> rng(0, availableQuestions[tier].size()-1);
     int questionIndex = rng(g);
